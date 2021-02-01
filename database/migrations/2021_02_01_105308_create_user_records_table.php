7a2271b5a2b7420f1d8ce54262b828aeeb152551
corprusCheese <samson20100@yandex.ru>
2021-02-01 14:30:56 +0300

    restyling

---
+++ database/migrations/2021_02_01_105308_create_user_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_records', function (Blueprint $table) {
+            $table->id('user_id')->index("user_idx");
+            $table->id('record_id')->index('record_idx');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_records');
+    }
+}
