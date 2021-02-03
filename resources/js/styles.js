f5b749afa95a5612911e9a6d4e796992a425c21f
corprusCheese <samson20100@yandex.ru>
2021-02-03 14:21:33 +0300

    header again

--- resources/js/styles.js
+++ resources/js/styles.js
@@ -3,21 +3,21 @@ import {HtmlResizer} from "./Classes/HtmlResizer";
 
 let htmlResizer = new HtmlResizer()
 let htmlTemplates = new HtmlTemplates()
-
+let navbar = $('#icon-navbar');
 
 // before actions
 htmlResizer.readySmall(() => {
-        $('#icon-navbar').html(htmlTemplates.getSmallHeaderSvg());
+        navbar.html(htmlTemplates.getSmallHeaderSvg());
     },
     () => {
-        $('#icon-navbar').html(htmlTemplates.getBigHeaderSvg());
+        navbar.html(htmlTemplates.getBigHeaderSvg());
     });
 
 // actions
 htmlResizer.resizeSmall(() => {
-        $('#icon-navbar').html(htmlTemplates.getSmallHeaderSvg());
+        navbar.html(htmlTemplates.getSmallHeaderSvg());
     },
     () => {
-        $('#icon-navbar').html(htmlTemplates.getBigHeaderSvg());
+        navbar.html(htmlTemplates.getBigHeaderSvg());
     })
 
--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,20 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" id='icon-navbar' href={{route('home')}}>
-        <svg viewBox="000 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg"
-             class="h-16 w-auto text-gray-700">
-        </svg>
-    </a>
-    <button id="togglerNav" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href={{route('login')}}>Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href={{route('register')}}>Register</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 10%; padding-right: 10%">
+    <div style="display: contents">
+        <a class="navbar-brand" id='icon-navbar' href={{route('home')}}>
+            <svg viewBox="000 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg"
+                 class="h-16 w-auto text-gray-700">
+            </svg>
+        </a>
+        <button id="togglerNav" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href={{route('login')}}>Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href={{route('register')}}>Register</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
